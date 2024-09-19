98055f817df304af0a0a2e793dca36228af8e8f9
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2024-09-19 17:26:21 +0200

    Image testing

--- src/components/PreviewsAndNewsContainer/PreviewsAndNewsContainer.styles.jsx
+++ src/components/PreviewsAndNewsContainer/PreviewsAndNewsContainer.styles.jsx
@@ -8,7 +8,6 @@ export const Container = styled.div`
     justify-content: center;
     margin: auto;
     gap: 3%;
-    background: url('https://i.ibb.co/pXMrr5v/back.jpg') center/cover no-repeat;
 
     @media screen and (max-width: 1100px) {
         display: flex;
