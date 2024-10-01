6b4ef47d277499d0ab32cd37fc0a1f8c41cf6e96
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2024-10-01 15:58:17 +0200

    Add Suspanse lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy} from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Loader from './components/Loader/Loader.component';
 
