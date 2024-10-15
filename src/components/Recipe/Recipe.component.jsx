7908a789d58a52d9aba53521d3540ea3d680d61c
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2024-10-15 16:24:38 +0200

    Add UseEffect Recipe.component

--- src/components/Recipe/Recipe.component.jsx
+++ src/components/Recipe/Recipe.component.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { RecipeContext } from '../../context/recipe.context.jsx';
