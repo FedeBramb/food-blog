d06a2c2c95d91a7fc4561ad0aea4b001eed622ad
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2024-10-26 16:42:12 +0200

    add style ScrollToTopButton

--- src/components/ScrollToTopButton/ScrollToTopButton.component.jsx
+++ src/components/ScrollToTopButton/ScrollToTopButton.component.jsx
@@ -1,7 +1,7 @@
 // ScrollToTopButton.jsx
 import React, { useState, useEffect } from 'react';
 
-import { Button } from './ScrollToTopButton.styles';
+import { Container } from './ScrollToTopButton.styles';
 
 const ScrollToTopButton = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -27,13 +27,23 @@ const ScrollToTopButton = () => {
   }, []);
 
   return (
-    <Button
+    <Container
       className={`scroll-to-top ${isVisible ? '' : 'hidden'}`}
       onClick={scrollToTop}
     >
-      ⬆️
-    </Button>
+      <svg className='arrow' viewBox="0 0 1792 1792"
+        width="46"       // Imposta larghezza 46px
+        height="46"      // Imposta altezza 46px
+        fill="currentColor" // Usa `currentColor` per applicare colore dal CSS
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path 
+          d="M1246 876q-8 20-30 20h-192v352q0 14-9 23t-23 9h-192q-14 0-23-9t-9-23v-352h-192q-14 0-23-9t-9-23q0-12 10-24l319-319q11-9 23-9t23 9l320 320q15 16 7 35zm-350-524q-148 0-273 73t-198 198-73 273 73 273 198 198 273 73 273-73 198-198 73-273-73-273-198-198-273-73zm768 544q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z">
+        </path>
+      </svg>
+    </Container>
   );
 };
 
 export default ScrollToTopButton;
+
--- src/components/ScrollToTopButton/ScrollToTopButton.styles.jsx
+++ src/components/ScrollToTopButton/ScrollToTopButton.styles.jsx
@@ -1,31 +1,31 @@
 import styled from "styled-components";
 
-export const Button = styled.button`
+export const Container = styled.div`
     position: fixed;
     bottom: 20px;
     right: 20px;
-    width: 50px;
-    height: 50px;
+    width: 46px;
+    height: 46px;
     border-radius: 50%;
-    background-color: #9AB973; /* Colore Verde Salvia */
-    color: white;
-    font-size: 24px;
     display: flex;
     align-items: center;
     justify-content: center;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.556);
     cursor: pointer;
-    border: none;
-    outline: none;
     z-index: 1000;
     transition: opacity 0.3s;
     
-    &:hover {
-        background-color: #7A9B6F;
-    }
-
     &.hidden {
         opacity: 0;
         pointer-events: none;
     }
+
+    .arrow {
+        color: #0078ab;
+
+        &:hover {
+            color: #174970; 
+        }
+    }
+
 `
